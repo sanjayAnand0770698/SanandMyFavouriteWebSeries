d4145cfdb252983a4dd93ed902530ea6dcfef2be
sanjayAnand0770698 <W0770698@myscc.ca>
2022-06-08 00:54:28 +0530

    Default image

---
+++ src/app/content-card/content-card.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Content } from '../models/content';
+
+
+@Component({
+  selector: 'app-content-card',
+  templateUrl: './content-card.component.html',
+  styleUrls: ['./content-card.component.scss']
+})
+export class ContentCardComponent implements OnInit {
+  @Input() contentItem?: Content;
+  @Input() isLastItem: boolean = false;
+
+  defaultImagePath = "assets/default-image.png";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  displayId(): void {
+    console.log('Current id is: ', this.contentItem?.id, this.contentItem?.title);
+  }
+}
