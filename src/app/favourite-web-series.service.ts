9975b7467035d9c37fb08da65b36cf63e4810e40
sanjayAnand0770698 <W0770698@myscc.ca>
2022-06-10 22:53:44 +0530

    Created methods for add,update,get list , get web series by id and remove web series

--- src/app/favourite-web-series.service.ts
+++ src/app/favourite-web-series.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { FAVOURITE_WEB_SERIES } from './data/mock-favourite-web-series';
+import { Content } from './models/content';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,60 @@ import { Injectable } from '@angular/core';
 export class FavouriteWebSeriesService {
 
   constructor() { }
+
+  getFavouriteWebSeriesList(): Observable<Content[]> {
+    const favourite_web_series = of(FAVOURITE_WEB_SERIES);
+    return favourite_web_series;
+  }
+
+  addFavouriteWebSeries(content: Content): Observable<Content[]> {
+    FAVOURITE_WEB_SERIES.push(content)
+    const finalList = of(FAVOURITE_WEB_SERIES);
+    return finalList;
+  }
+
+  getFavouriteWebSeriesContent(id: number): Content | undefined {
+    let contentItemIndex = FAVOURITE_WEB_SERIES.findIndex((data) => data.id = id);
+    if (contentItemIndex != -1) {
+      return FAVOURITE_WEB_SERIES[contentItemIndex];
+    } return;
+  }
+
+  updateFavouriteWebSeries(content: Content): Observable<Content[]> {
+
+    let contentItemIndex = FAVOURITE_WEB_SERIES.findIndex((data) => data.id = content.id);
+    if (contentItemIndex != -1) {
+      if (content.id)
+        FAVOURITE_WEB_SERIES[contentItemIndex].id = content.id;
+      if (content.title)
+        FAVOURITE_WEB_SERIES[contentItemIndex].title = content.title;
+      if (content.body)
+        FAVOURITE_WEB_SERIES[contentItemIndex].body = content.body;
+      if (content.author)
+        FAVOURITE_WEB_SERIES[contentItemIndex].author = content.author;
+      if (content.imagelink)
+        FAVOURITE_WEB_SERIES[contentItemIndex].imagelink = content.imagelink;
+      if (content.type)
+        FAVOURITE_WEB_SERIES[contentItemIndex].type = content.type;
+      if (content.hashtags)
+        FAVOURITE_WEB_SERIES[contentItemIndex].hashtags = content.hashtags;
+      let data = [...FAVOURITE_WEB_SERIES, content];
+      const finalList = of(data);
+      return finalList;
+    } else {
+      const finalList = of(FAVOURITE_WEB_SERIES);
+      return finalList;
+    }
+
+  }
+
+  removeFavouriteWebSeries(id: number): Content | undefined {
+    let contentItemIndex = FAVOURITE_WEB_SERIES.findIndex((data) => data.id = id);
+    if (contentItemIndex != -1) {
+      let contentItem = FAVOURITE_WEB_SERIES[contentItemIndex];
+      FAVOURITE_WEB_SERIES.splice(0, contentItemIndex);
+      return contentItem;
+    } return;
+  }
+
 }
