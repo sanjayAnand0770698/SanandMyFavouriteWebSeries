fbbce17f6fda3bd9431323610c814602687ca8ae
sanjayAnand0770698 <w0770698@myscc.ca>
2022-07-04 21:42:32 +0530

    completed the content search component functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FavouriteWebSeriesService } from './favourite-web-series.service';
-import { Content } from './models/content';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,29 +6,12 @@ import { Content } from './models/content';
 })
 export class AppComponent implements OnInit {
   title = 'SanandMyFavouriteWebSeries';
-  result?: string;
-  content?: Content;
-  searchedItemId?: number;
-  constructor(private favouriteWebSeriesService: FavouriteWebSeriesService) {
 
+  constructor() {
   }
 
   ngOnInit(): void {
-
   }
 
-  getFilteredWebSeries(id: string): void {
-    if (id) {
-      this.favouriteWebSeriesService.getFavouriteWebSeriesContent(parseInt(id))
-        .subscribe(series => {
-          console.log(series)
-          this.content = series;
-          this.searchedItemId = series.id;
-        });
-    } else {
-      alert("Please Enter id to filter content");
-    }
-
-  }
 }
 
--- src/app/content-search/content-search.component.ts
+++ src/app/content-search/content-search.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FavouriteWebSeriesService } from '../favourite-web-series.service';
+import { Content } from '../models/content';
 @Component({
   selector: 'app-content-search',
   templateUrl: './content-search.component.html',
   styleUrls: ['./content-search.component.scss']
 })
 export class ContentSearchComponent implements OnInit {
-
-  constructor() { }
+  title = 'SanandMyFavouriteWebSeries';
+  result?: string;
+  content?: Content;
+  isUserTyped: boolean = false;
+  constructor(private favouriteWebSeriesService: FavouriteWebSeriesService) { }
 
   ngOnInit(): void {
+  }
+
+  getFilteredWebSeries(id: string): void {
+    if (id) {
+      this.favouriteWebSeriesService.getFavouriteWebSeriesContent(parseInt(id))
+        .subscribe(content => {
+          if (content.id !== -1) {
+            this.content = content;
+          } else {
+            this.content = undefined;
+          }
+        });
+    } else {
+      alert("Please Enter id to filter content");
+    }
   }
 
 }
